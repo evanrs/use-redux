301d59e592befd4820875c4d4d13774aca72659e
Evan Schneider <evanrs@gmail.com>
2015-09-01 19:59:12 -0400

    Dispatch/subscribe redux store.

--- index.js
+++ index.js
@@ -3,6 +3,27 @@ import 'babel-core/polyfill';
 import React from 'react';
 
 class Root extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  setClicks() {
+    this.setState({
+      clicks: store.getState()
+    })
+  }
+
+  componentWillMount() {
+    this.setClicks();
+    this.unsubscribe = store.subscribe(this.setClicks.bind(this))
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+
   render() {
     let style = {
       fontSize: 100,
@@ -12,10 +33,31 @@ class Root extends React.Component {
 
     return (
       <div style={style}>
-        💯
+        <span
+          onClick={event => store.dispatch(increment())}>
+          {Array(this.state.clicks + 1).fill().map(v => "💯")}
+        </span>
       </div>
     )
   }
 }
+
+import {createStore} from 'redux';
+
+function counter(state = 0, action) {
+  switch (action.type) {
+    case 'INCREMENT': return state + 1;
+    case 'DECREMENT': return state - 1;
+    default: return state;
+  }
+}
+
+const store = createStore(counter);
+
+function increment () {
+  return {type: 'INCREMENT'};
+}
+
+// import {Provider} from 'react-redux';
 
 React.render(<Root />, document.getElementById('root'));
