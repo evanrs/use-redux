581b84dff54a87c45f09fb78d9c12ea837f5b287
Evan Schneider <evanrs@gmail.com>
2015-09-01 23:04:18 -0400

    Implement simple todo example.

--- index.js
+++ index.js
@@ -1,20 +1,55 @@
 import 'babel-core/polyfill';
-
 import React from 'react';
 
+// import Root from './components/root';
+
 class Root extends React.Component {
+  static propTypes = {
+    todos: React.PropTypes.array
+  }
+
+  static defaultProps = {
+    todos: []
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
   render() {
     let style = {
-      fontSize: 100,
+      fontSize: 20,
       position: 'fixed', top: '50%', left: '50%',
       transform: 'translate(-50%, -50%)',
       maxWidth: '100%'
     }
 
     return (
-      <div style={style}
-           onClick={event => this.props.dispatch(increment())}>
-        {Array(this.props.count + 1).fill().map(v => " 💯 ")}
+      <div style={style}>
+        <form
+          onSubmit={event => {
+            event.preventDefault();
+            this.props.dispatch(addTodo(this.state.text));
+            this.setState({text: ''});
+          }}
+        >
+          <input
+            value={this.state.text}
+            onChange={event => this.setState({text: event.target.value})}
+          />
+        </form>
+        {this.props.todos.map(todo => (
+          <div>
+            {todo}
+            <span
+              onClick={event => this.props.dispatch(removeTodo(todo))}
+            >
+              &nbsp;×
+            </span>
+          </div>
+        ))}
       </div>
     )
   }
@@ -22,19 +57,38 @@ class Root extends React.Component {
 
 import {createStore} from 'redux';
 
-function counter(state = {count: 0}, action) {
+function addTodo(text) {
+  return {type: 'ADD_TODO', text};
+}
+
+function removeTodo(text) {
+  return {type: 'REMOVE_TODO', text}
+}
+
+function todos(state = {todos: ['howdy']}, action) {
   switch (action.type) {
-    case 'INCREMENT': return {count: state.count + 1};
-    case 'DECREMENT': return {count: state.count - 1};
-    default: return state;
+    case 'ADD_TODO':
+      return {todos: [action.text, ...state.todos]};
+    case 'REMOVE_TODO':
+      return {todos: state.todos.filter(todo => todo !== action.text)};
+    default:
+      return state;
   }
 }
 
-const store = createStore(counter);
+// function counter(state = {count: 0}, action) {
+//   switch (action.type) {
+//     case 'INCREMENT': return {count: state.count + 1};
+//     case 'DECREMENT': return {count: state.count - 1};
+//     default: return state;
+//   }
+// }
 
-function increment () {
-  return {type: 'INCREMENT'};
-}
+// function increment () {
+//   return {type: 'INCREMENT'};
+// }
+
+const store = createStore(todos);
 
 import {Provider, connect} from 'react-redux';
 
