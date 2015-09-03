f04b93b50404209a798002e16bd048af370c2a29
Evan Schneider <evanrs@gmail.com>
2015-09-03 17:32:57 -0400

    Handle dehydrated init state.

--- reducers/filter.js
+++ reducers/filter.js
@@ -3,18 +3,27 @@ import actions from '../actions';
 const { FILTER_NONE, FILTER_COMPLETE, FILTER_INCOMPLETE } = actions.filter;
 
 const identity = v => v;
+const filterComplete = todo => !! todo.complete;
+const filterIncomplete = todo => ! todo.complete;
+
 const initialState = {type: FILTER_NONE, test: identity};
 
 function filter(state=initialState, {type}) {
+    if (! state.test) {
+      type = state.type;
+    }
+
     switch (type) {
+      case state.type:
+        type = FILTER_NONE;
+      case FILTER_NONE:
+        return {type, test: identity};
+
       case FILTER_COMPLETE:
         return {type, test: todo => !! todo.complete};
 
       case FILTER_INCOMPLETE:
         return {type, test: todo => ! todo.complete};
-
-      case FILTER_NONE:
-        return {type, test: identity};
 
       default:
         return state;
