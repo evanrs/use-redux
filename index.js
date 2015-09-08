9b70ecdb4c52f66a953a1b3c1a687c3158cc8095
Evan Schneider <evanrs@gmail.com>
2015-09-08 19:29:31 -0400

    Switch webpack dev middleware

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-import less from 'less';
 import 'babel-core/polyfill';
 
 import React from 'react';
 
 import App from './containers/App';
+
+require('./static/style.less');
 
 React.render(<App />, document.getElementById('root'));
 
--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,9 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  devtool: 'cheap-module-eval-source-map',
+  devtool: 'eval',
   entry: [
-    'webpack-dev-server/client?http://localhost:3000',
-    'webpack/hot/only-dev-server',
+    'webpack-hot-middleware/client',
     './index'
   ],
   output: {
@@ -14,13 +13,20 @@ module.exports = {
     publicPath: '/static/'
   },
   plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin()
   ],
   module: {
     loaders: [{
       test: /\.js$/,
-      loaders: ['react-hot', 'babel'],
+      loaders: ['babel'],
+      exclude: /node_modules/,
+      include: __dirname
+    },
+    {
+      test: /\.less$/,
+      loaders: ['style', 'css', 'autoprefixer', 'less'],
       exclude: /node_modules/,
       include: __dirname
     }]
