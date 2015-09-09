94a88263bf1f4ee6b695b2b5813152edee9dd667
Evan Schneider <evanrs@gmail.com>
2015-09-09 13:28:18 -0400

    Add DRAFT_TODO action

--- actions/todos.js
+++ actions/todos.js
@@ -1,9 +1,17 @@
 export const ADD_TODO = 'ADD_TODO';
 export const TOGGLE_TODO = 'TOGGLE_TODO';
 export const REMOVE_TODO = 'REMOVE_TODO';
+export const DRAFT_TODO = 'DRAFT_TODO';
 
-export function addTodo(text) {
-  return {type: ADD_TODO, text};
+export function draftTodo(todo, text) {
+  if (! text || ! text.length)
+    return removeTodo(text);
+
+  return {type: DRAFT_TODO, todo, text};
+}
+
+export function addTodo(todo) {
+  return {type: ADD_TODO, todo};
 }
 
 export function toggleTodo (todo) {
