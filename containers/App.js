d03df62534c7028853478d610a521ac7cead6e16
Evan Schneider <evanrs@gmail.com>
2015-09-10 18:01:36 -0400

    Add app appear animation

--- containers/App.js
+++ containers/App.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react/addons';
+const {CSSTransitionGroup} = React.addons;
 
 import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
 import { devTools, persistState } from 'redux-devtools';
 import { DevTools, DebugPanel, LogMonitor } from 'redux-devtools/lib/react';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
-
 import TodoApp from './TodoApp';
 
 const store = require('../store')();
@@ -13,7 +13,7 @@ const store = require('../store')();
 class App extends Component {
   render() {
     return (
-      <div>
+      <CSSTransitionGroup transitionName="app" transitionAppear={true}>
         <Provider store={store}>
           {() => <TodoApp/>}
         </Provider>
@@ -24,7 +24,7 @@ class App extends Component {
               monitor={LogMonitor} />
           </DebugPanel>
         }
-      </div>
+      </CSSTransitionGroup>
     )
   }
 }
