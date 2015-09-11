6fed4317f9eb69ff52f3a2afefbe689192d66f71
Evan Schneider <evanrs@gmail.com>
2015-09-11 15:15:41 -0400

    Move inline style to less

--- components/TodoItem.js
+++ components/TodoItem.js
@@ -2,15 +2,6 @@ import React, { Component, PropTypes } from 'react/addons';
 const {CSSTransitionGroup} = React.addons;
 
 export default class TodoItem extends Component {
-  static style = {
-    position: 'relative',
-    display: 'flex',
-    width: '100%',
-    alignItems: 'center',
-    fontSize: 16,
-    fontWeight: 100
-  }
-
   componentWillReceiveProps(nextProps) {
     this.setState({active: void 0});
   }
@@ -25,7 +16,7 @@ export default class TodoItem extends Component {
     return (
       <div>
         <CSSTransitionGroup transitionName="todoList" transitionAppear={true}>
-          <div key={item.id} style={TodoItem.style}>
+          <div key={item.id} className="todoitem">
             <div
               className={`check ${active ? '' : 'in'}active`}
               onClick={event => (
@@ -37,12 +28,14 @@ export default class TodoItem extends Component {
                 <path d="M56.560,-0.010 C37.498,10.892 26.831,26.198 20.617,33.101 C20.617,33.101 5.398,23.373 5.398,23.373 C5.398,23.373 0.010,29.051 0.010,29.051 C0.010,29.051 24.973,51.981 24.973,51.981 C29.501,41.166 42.502,21.583 60.003,6.565 C60.003,6.565 56.560,-0.010 56.560,-0.010 Z" id="path-1" className="cls-2" fill-rule="evenodd"/>
               </svg>
             </div>
-            <div style={{flex: 0.7, padding: 16, fontSize: 13, opacity: item.complete ? 0.5 : 1}}>{item.text}</div>
+            <div className={`todoitem-text ${item.complete && 'todoitem-text__complete'}`}>
+              {item.text}
+            </div>
             <input
+              className="todoitem-delete"
               type="button"
               value="&nbsp;×&nbsp;"
               onClick={event => onDelete(item)}
-              style={{color: '#FFF', background: 'none'}}
             />
           </div>
         </CSSTransitionGroup>
