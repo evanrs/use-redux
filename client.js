0b0c4a4e640378a862efcc78fa21a278aee59d90
Evan Schneider <evanrs@gmail.com>
2015-09-11 21:10:06 -0400

    Enable touch events

--- client.js
+++ client.js
@@ -6,6 +6,7 @@ require.ensure(['react', './containers/App'], function (require) {
   const React = require('react');
   const App = require('./containers/App');
 
+  React.initializeTouchEvents(true);
   React.render(<App />, document.getElementById('root'));
 });
 
