a825be5c01d3b56593afccdd8fbfffe18080e7ed
Evan Schneider <evanrs@gmail.com>
2015-09-14 13:07:06 -0400

    Add ToggleAnimationComponent

--- components/DebugAnimationComponent.js
+++ components/DebugAnimationComponent.js
@@ -16,3 +16,31 @@ class DebugAnimationComponent extends Component {
 }
 
 export default DebugAnimationComponent;
+
+export class ToggleAnimationComponent extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {_toggleAnimation: true, ...this.state};
+
+    this._render = this.render;
+    this.render = this.toggleAnimationRender;
+
+    this._toggleAnimationInterval = global.setInterval(() => {
+      this.toggleAnimationToggle();
+      global.setTimeout(this.toggleAnimationToggle.bind(this), 300)
+    }, 2000);
+  }
+
+  componentWillUnmount() {
+    global.clearInterval(this._toggleAnimationInterval)
+  }
+
+  toggleAnimationToggle() {
+    this.setState({_toggleAnimation: ! this.state._toggleAnimation})
+  }
+
+  toggleAnimationRender() {
+    return this.state._toggleAnimation ? this._render() : <span/>;
+  }
+}
