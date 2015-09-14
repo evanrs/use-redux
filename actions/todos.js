35fc6b1179bfe7821aa896d68af223832081198e
Evan Schneider <evanrs@gmail.com>
2015-09-14 13:04:03 -0400

    Add edit, save actions

--- actions/todos.js
+++ actions/todos.js
@@ -1,7 +1,9 @@
+export const DRAFT_TODO = 'DRAFT_TODO';
 export const ADD_TODO = 'ADD_TODO';
+export const EDIT_TODO = 'EDIT_TODO';
+export const SAVE_TODO = 'SAVE_TODO';
 export const TOGGLE_TODO = 'TOGGLE_TODO';
 export const REMOVE_TODO = 'REMOVE_TODO';
-export const DRAFT_TODO = 'DRAFT_TODO';
 
 export function draftTodo(id, text) {
   return {type: DRAFT_TODO, id, text};
@@ -9,6 +11,14 @@ export function draftTodo(id, text) {
 
 export function addTodo(id, text) {
   return {type: ADD_TODO, id, text};
+}
+
+export function editTodo(id, text) {
+  return {type: EDIT_TODO, id};
+}
+
+export function saveTodo(id, text) {
+  return {type: SAVE_TODO, id, text}
 }
 
 export function toggleTodo (id) {
