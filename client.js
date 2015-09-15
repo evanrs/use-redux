817b83a343a28ca4b8604568bff81697260e5c74
Evan Schneider <evanrs@gmail.com>
2015-09-14 22:12:30 -0400

    Add demo date.

--- client.js
+++ client.js
@@ -2,6 +2,10 @@ import 'babel-core/polyfill';
 
 require('./style');
 
+if (! localStorage.getItem('_actionHistory')) {
+  localStorage.setItem('_actionHistory', JSON.stringify(require('./demo')));
+}
+
 require.ensure(['react', './containers/App'], function (require) {
   const React = require('react');
   const App = require('./containers/App');
---
+++ demo.js
@@ -0,0 +1,720 @@
+module.exports = [
+    {
+        "type": "FILTER_COMPLETE"
+    },
+    {
+        "type": "FILTER_INCOMPLETE"
+    },
+    {
+        "type": "TOGGLE_TODO",
+        "id": 8
+    },
+    {
+        "type": "ADD_TODO",
+        "id": 9,
+        "text": "Share with friends"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 9,
+        "text": "Share with friends"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 9,
+        "text": "Share with friends"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 9,
+        "text": "Share with friends"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 9,
+        "text": "Share with fr"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 9,
+        "text": "Share with "
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 9,
+        "text": "Share "
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 9,
+        "text": "S"
+    },
+    {
+        "type": "ADD_TODO",
+        "id": 8,
+        "text": "Add gratuitous time travel scrubber"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 8,
+        "text": "Add gratuitous time travel scrubber"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 8,
+        "text": "Add gratuitous time travel scru"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 8,
+        "text": "Add gratuitous time travel sc"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 8,
+        "text": "Add gratuitous time travel "
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 8,
+        "text": "Add gratuitous time tra"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 8,
+        "text": "Add gratuitous time tr"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 8,
+        "text": "Add gratuitous time "
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 8,
+        "text": "Add gratuitous time"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 8,
+        "text": "Add gratuitous "
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 8,
+        "text": "Add gratuit"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 8,
+        "text": "Add gratu"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 8,
+        "text": "Add grat"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 8,
+        "text": "Add gra"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 8,
+        "text": "Add "
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 8,
+        "text": "Add"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 8,
+        "text": "A"
+    },
+    {
+        "type": "TOGGLE_TODO",
+        "id": 7
+    },
+    {
+        "type": "TOGGLE_TODO",
+        "id": 6
+    },
+    {
+        "type": "TOGGLE_TODO",
+        "id": 5
+    },
+    {
+        "type": "FILTER_INCOMPLETE"
+    },
+    {
+        "type": "ADD_TODO",
+        "id": 7,
+        "text": "Let the future be rewritten"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 7,
+        "text": "Let the future be rewritten"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 7,
+        "text": "Let the future be rewritten"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 7,
+        "text": "Let the future be rew"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 7,
+        "text": "Let the future be re"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 7,
+        "text": "Let the future be r"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 7,
+        "text": "Let the future be "
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 7,
+        "text": "Let the future "
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 7,
+        "text": "Let the fut"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 7,
+        "text": "Let the f"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 7,
+        "text": "Let the "
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 7,
+        "text": "Let the"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 7,
+        "text": "Let "
+    },
+    {
+        "type": "TOGGLE_TODO",
+        "id": 4
+    },
+    {
+        "type": "ADD_TODO",
+        "id": 6,
+        "text": "Add scrubbers to walk through time"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 6,
+        "text": "Add scrubbers to walk through time"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 6,
+        "text": "Add scrubbers to walk through "
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 6,
+        "text": "Add scrubbers to walk "
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 6,
+        "text": "Add scrubbers to walk"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 6,
+        "text": "Add scrubbers to "
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 6,
+        "text": "Add scrubbers "
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 6,
+        "text": "Add scrubbers"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 6,
+        "text": "Add scru"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 6,
+        "text": "Add sc"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 6,
+        "text": "Add "
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 6,
+        "text": "A"
+    },
+    {
+        "type": "TOGGLE_TODO",
+        "id": 3
+    },
+    {
+        "type": "ADD_TODO",
+        "id": 5,
+        "text": "Time travel with cmd+z"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 5,
+        "text": "Time travel with cmd+z"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 5,
+        "text": "Time travel with cmd+z"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 5,
+        "text": "Time travel with cmd+"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 5,
+        "text": "Time travel with cmd"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 5,
+        "text": "Time travel with "
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 5,
+        "text": "Time travel "
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 5,
+        "text": "Time travel"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 5,
+        "text": "Time tra"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 5,
+        "text": "Time tr"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 5,
+        "text": "Time "
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 5,
+        "text": "T"
+    },
+    {
+        "type": "ADD_TODO",
+        "id": 4,
+        "text": "Record history of user actions"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 4,
+        "text": "Record history of user actions"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 4,
+        "text": "Record history of user actions"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 4,
+        "text": "Record history of user ac"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 4,
+        "text": "Record history of user "
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 4,
+        "text": "Record history of user"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 4,
+        "text": "Record history of us"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 4,
+        "text": "Record history of "
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 4,
+        "text": "Record history "
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 4,
+        "text": "Record histor"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 4,
+        "text": "Record hist"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 4,
+        "text": "Record "
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 4,
+        "text": "Rec"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 4,
+        "text": "Re"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 4,
+        "text": "R"
+    },
+    {
+        "type": "ADD_TODO",
+        "id": 3,
+        "text": "Send input events through actions"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 3,
+        "text": "Send input events through actions"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 3,
+        "text": "Send input events through actions"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 3,
+        "text": "Send input events through action"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 3,
+        "text": "Send input events through ac"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 3,
+        "text": "Send input events through "
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 3,
+        "text": "Send input events throu"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 3,
+        "text": "Send input events t"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 3,
+        "text": "Send input events "
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 3,
+        "text": "Send input event"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 3,
+        "text": "Send input "
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 3,
+        "text": "Send in"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 3,
+        "text": "Send "
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 3,
+        "text": "S"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 3,
+        "text": ""
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 3,
+        "text": "P"
+    },
+    {
+        "type": "TOGGLE_TODO",
+        "id": 2
+    },
+    {
+        "type": "ADD_TODO",
+        "id": 2,
+        "text": "Create a todo app using redux"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 2,
+        "text": "Create a todo app using redux"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 2,
+        "text": "Create a todo app using redux"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 2,
+        "text": "Create a todo app using re"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 2,
+        "text": "Create a todo app using "
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 2,
+        "text": "Create a todo app "
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 2,
+        "text": "Create a todo app"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 2,
+        "text": "Create a todo a"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 2,
+        "text": "Create a todo "
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 2,
+        "text": "Create a todo"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 2,
+        "text": "Create a "
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 2,
+        "text": "Create "
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 2,
+        "text": "C"
+    },
+    {
+        "type": "TOGGLE_TODO",
+        "id": 1
+    },
+    {
+        "type": "ADD_TODO",
+        "id": 1,
+        "text": "Read about redux"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 1,
+        "text": "Read about redux"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 1,
+        "text": "Read about redux"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 1,
+        "text": "Read about red"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 1,
+        "text": "Read about r"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 1,
+        "text": "Read about "
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 1,
+        "text": "Read ab"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 1,
+        "text": "Read "
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 1,
+        "text": "R"
+    },
+    {
+        "type": "TOGGLE_TODO",
+        "id": 0
+    },
+    {
+        "type": "ADD_TODO",
+        "id": 0,
+        "text": "Watch Dan Abramovs talk on redux"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 0,
+        "text": "Watch Dan Abramovs talk on redux"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 0,
+        "text": "Watch Dan Abramovs talk on redux"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 0,
+        "text": "Watch Dan Abramovs talk on re"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 0,
+        "text": "Watch Dan Abramovs talk on "
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 0,
+        "text": "Watch Dan Abramovs talk "
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 0,
+        "text": "Watch Dan Abramovs ta"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 0,
+        "text": "Watch Dan Abramovs t"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 0,
+        "text": "Watch Dan Abramovs "
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 0,
+        "text": "Watch Dan Abramovs"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 0,
+        "text": "Watch Dan Abramov"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 0,
+        "text": "Watch Dan Abra"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 0,
+        "text": "Watch Dan A"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 0,
+        "text": "Watch Dan "
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 0,
+        "text": "Watch "
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 0,
+        "text": "Watc"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 0,
+        "text": "Wat"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 0,
+        "text": "Wa"
+    },
+    {
+        "type": "DRAFT_TODO",
+        "id": 0,
+        "text": "W"
+    },
+    {
+        "type": "BEGINNING_OF_TIME"
+    }
+]
