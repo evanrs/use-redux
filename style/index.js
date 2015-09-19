41f44eb85e2ab4d830667c3b0535ae033d0639ae
Evan Schneider <evanrs@gmail.com>
2015-09-18 23:52:36 -0400

    Pivot style on domain

--- style/index.js
+++ style/index.js
@@ -1,1 +1,7 @@
-require('./style.less');
+
+if (/green/.test(location.href))
+  require('./mint-theme.less');
+else if (/red/.test(location.href))
+  require('./red-theme.less');
+else
+  require('./blue-theme.less')
